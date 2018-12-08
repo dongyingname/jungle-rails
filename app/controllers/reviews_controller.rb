2b34b63e596d3399a16610dc78c3fd4b35c8dd9e
Ying Dong <dongyingname@yahoo.com>
2018-12-08 12:02:48 -0700

    reviews_controller: -Declare @product so it can be passed back to product/show if @review.save fails.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,19 @@
 class ReviewsController < ApplicationController
 
   def create
-    
+    @product=Product.find params[:product_id]
     @review = Review.new(review_params)
     @review.user = current_user
-    @review.product = Product.find params[:product_id]
+    @review.product = @product
 
     puts "review_params", review_params.inspect
     if @review.save
       redirect_to '/'
+    else
+      render template: "products/show"
     end
+
+
   end
 
   private
