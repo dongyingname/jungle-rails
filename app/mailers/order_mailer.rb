bc99b8f82b7a6b8f4ca406fc287312c48dd15fbb
Ying Dong <dongyingname@yahoo.com>
2018-12-07 18:00:29 -0700

    order_mailer: Takes @order that is passed from order_controller and assign those into variables available to views. confirm_order views: render order information dynamically.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,9 +3,11 @@ class OrderMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
  
   def confirm_order(order)
-
+    @email = order.email
+    @sub_total_cents = order.total_cents
+    @order_id = order.id
     
-    mail(to: 'dongyingname@yahoo.com', subject: 'Welcome to My Awesome Site')
+    mail(to: @email, subject: "Order Receipt from Jungle Website: Order ##{@order_id}")
   end
 
 end
