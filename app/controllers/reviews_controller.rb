8f33bcbfaabda43187ebddb4e51461cdb1c84885
Ying Dong <dongyingname@yahoo.com>
2018-12-08 11:48:18 -0700

    reviews_controller: -change the way of defining review_params, making it cleaner. -link review to the product by @review.product.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,11 @@
 class ReviewsController < ApplicationController
 
   def create
+    
     @review = Review.new(review_params)
     @review.user = current_user
+    @review.product = Product.find params[:product_id]
+
     puts "review_params", review_params.inspect
     if @review.save
       redirect_to '/'
@@ -12,11 +15,12 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    {
-      product_id: params[:product_id],
-      rating: params.require(:review)[:rating],
-      description: params.require(:review)[:description]
-    }
+
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
+  
   end
 
 
