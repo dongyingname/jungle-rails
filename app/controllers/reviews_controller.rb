d7a2205476c1c4905b91a5edbddbf820afeb103e
Ying Dong <dongyingname@yahoo.com>
2018-12-08 11:26:55 -0700

    reviews_controller: move review_params into a private method.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,45 +1,23 @@
 class ReviewsController < ApplicationController
 
   def create
-    puts "product_id", product_id = params[:product_id]
-    puts "user_id",user_id = session[:user_id]
-    puts "email", email = session[:email]
-    puts "description", description = params.require(:review)[:description]
-    puts "rating", rating = params.require(:review)[:rating]
-
-    review_params = {
-                     product_id: product_id,
-                     user_id: user_id,
-                     rating: rating,
-                     description: description
-    }
     @review = Review.new(review_params)
+    puts "review_params", review_params.inspect
     if @review.save
-      
       redirect_to '/'
     end
-
-
-
-    # })
-    # puts 'SAVED?', @review.save
-
-    # @review.user = current_user
-    # raise "Yay, I'm here!"
-    # if @review.save
-      # redirect_to '/', notice: 'Product created!'
-    # else
-    # render 
-    # end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(
-      :description,
-      :rating
-    )
+    # puts "email", email = session[:email]
+    {
+      product_id: params[:product_id],
+      user_id: session[:user_id],
+      rating: params.require(:review)[:rating],
+      description: params.require(:review)[:description]
+    }
   end
 
 
