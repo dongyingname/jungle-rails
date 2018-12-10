098b3e252ef438644b65e8b20b6f1779f8a4a90d
Ying Dong <dongyingname@yahoo.com>
2018-12-10 15:06:27 -0700

    User model: Add validation to a minimum password length of 6.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true 
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :password, length: { minimum: 6 }
   validates :password_digest, presence: true
 
   
